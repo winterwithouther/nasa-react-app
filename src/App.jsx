6ed08bcdf50f5cc868dac21589cb7e7bb8347286
EmanGurung <136867625+winterwithouther@users.noreply.github.com>
2024-07-30 18:47:09 -0400

    fetching data from Nasa API and set up the footer with requested data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 import Footer from "./components/Footer"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
   const [showModel, setModel] = useState(false)
+  
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+      const url = "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`
+
+      try {
+        const res = await fetch(url)
+        const apiData = await res.json()
+        setData(apiData)
+        console.log("DATA\n", apiData)
+      } catch (err) {
+        console.log(err.message)
+      }
+    }
+
+    fetchAPIData()
+  }, [])
 
   function handleToggleModel() {
     setModel(!showModel)
   }
   return (
     <>
-      <Main/>
-      {showModel && (<SideBar handleToggleModel={handleToggleModel}/>)}
-      <Footer handleToggleModel={handleToggleModel}/>
+      {data ? (<Main/>) : (
+        <div className="loading-state">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}
+      {showModel && (<SideBar data={data} handleToggleModel={handleToggleModel}/>)}
+      {data && (<Footer data={data} handleToggleModel={handleToggleModel}/>)}
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer(props) {
-    const {handleToggleModel} = props
+    const {handleToggleModel, data} = props
     return (
         <footer>
             <div className="background-gradient">
 
             </div>
             <div>
-                <h2>The Brutal Martian Landscape</h2>
+                <h2>{data?.title}</h2>
                 <h1>APOD PROJECT</h1>
             </div>
             <button onClick={handleToggleModel}>
